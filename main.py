d28ac076b0f9790603d9bb868325276a0eb85a92
jomrembau <jomir.bautista@gmail.com>
2026-07-30 09:38:08 +0200

    Flask Basic reviews

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask("__name__")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/<name>")
+def welcome(name):
+    return render_template("welcome.html", name=name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
