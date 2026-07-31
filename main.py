5cb81a45673246fbef9e8c9a3164585d9cfb863e
jomrembau <jomir.bautista@gmail.com>
2026-07-31 12:17:10 +0200

    flask template forms

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -13,6 +13,16 @@ def about():
 @app.route("/<name>")
 def welcome(name):
     return render_template("welcome.html", name=name)
+
+@app.route("/login", methods=("GET","POST"))
+def login():
+    return render_template("login.html")
+
+@app.route("/welcome")
+def authenticated():
+    email = request.args.get("email")
+    print("EMail", email)
+    return render_template("authenticated.html", email=email)
 
 if __name__ == "__main__":
     app.run(debug=True)
