d574b01168d2c80eb199fcec144ecbe956b9dc47
jomrembau <jomir.bautista@gmail.com>
2026-08-03 10:25:50 +0200

    Forms Fields part 1

--- forms_practice/exer_wtf.py
+++ forms_practice/exer_wtf.py
@@ -1,26 +1,40 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, session, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import SubmitField, StringField, BooleanField, DateTimeField, SelectField, TextAreaField, RadioField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "mysecretkey"
 
 class InfoForm(FlaskForm):
-    breed = StringField("Breed Stringfield")
+    breed = StringField("Breed?", validators=[DataRequired()])
+    neuter = BooleanField("neutered?")
+    mood = RadioField("Theme?",
+                      choices=[("mood_one","happy"),("mood_two","excited")])
+    food_choice = SelectField("Pick Favorite food:", choices=[("ch","chicken"),("bf","beef"),("fish","Fish")])
+    feedback = TextAreaField()
     submit = SubmitField("Submit")
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    breed = False
 
     form = InfoForm()
 
     if form.validate_on_submit():
-        breed = form.breed.data
-        form.breed.data = ""
+        session["breed"] = form.breed.data
+        session["neuter"] = form.neuter.data
+        session["mood"] = form.mood.data
+        session["food_choice"] = form.food_choice.data
+        session["feedback"] = form.feedback.data
 
-    return render_template("index.html",form=form, breed=breed)
+        return redirect(url_for("thankyou"))
+
+    return render_template("index.html", form=form)
+
+@app.route("/thankyou")
+def thankyou():
+    return  render_template("thankyou.html")
 
 
 
