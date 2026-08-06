425909b0fe8ef5e631c6f0e81890cff7637b58a4
jomrembau <jomir.bautista@gmail.com>
2026-08-06 10:07:13 +0200

    Flask Relationship part1

---
+++ Database/Database Models/models.py
@@ -0,0 +1,68 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///"+os.path.join(basedir,"data.sqlite")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+Migrate(app,db)
+
+class Puppies(db.Model):
+    __tablename__ = "puppies"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    toys = db.relationship("Toy", backref="puppy", lazy="dynamic")
+    # uselist=False because each puppy can have only one owner (one-to-one relationship)
+    owner = db.relationship("Owner",backref="puppy", uselist=False)
+
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        if self.owner:
+            return f"Puppy name: {self.name} - Owner: {self.owner.name}"
+        else:
+            return f"Puppy name: {self.name} - Currently no Owner"
+
+    def report_toys(self):
+        print("Toys List:")
+        for toy in self.toys:
+            print(toy.item_name)
+
+
+class Toy(db.Model):
+    __tablename__ = "toys"
+
+    id = db.Column(db.Integer, primary_key=True)
+    item_name = db.Column(db.Text)
+    #From Puppy Class
+    puppy_id = db.Column(db.Integer,db.ForeignKey("puppies.id"))
+
+    def __init__(self, item_name, puppy_id):
+        self.item_name = item_name
+        self.puppy_id = puppy_id
+
+
+class Owner(db.Model):
+    __tablename__ = "owners"
+
+    id = db.Column(db.Integer, primary_key=True)
+    owner_name = db.Column(db.Text)
+    puppy_id = db.Column(db.Integer, db.ForeignKey("puppies.id"))
+
+    def __init__(self, item_name, puppy_id):
+        self.item_name = item_name
+        self.puppy_id = puppy_id
+
+
+if __name__ == "__main__":
+     app.run(debug=True)
