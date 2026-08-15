1ea8ca0bdf2231620ff56b65b873ecabfe0c48fc
jomrembau <jomir.bautista@gmail.com>
2026-08-15 11:38:09 +0200

    Delete Page

--- large app prac/project/students/views.py
+++ large app prac/project/students/views.py
@@ -2,7 +2,8 @@ from flask import render_template, url_for,redirect, flash
 from project import db
 from project.models import Students
 from project.students import student_blueprint
-from project.students.forms import AddForm
+from project.students.forms import AddForm, DeleteForm
+
 
 @student_blueprint.route("/add", methods=["GET","POST"])
 def add_student():
@@ -15,10 +16,28 @@ def add_student():
         new_student = Students(student_name,class_section)
 
         db.session.add(new_student)
+        db.session.commit()
 
         flash('Student Successfully added.')
 
         return redirect(url_for("students.add_student"))
 
     return render_template("add.html", form=form)
+
+@student_blueprint.route("/delete", methods=["GET","POST"])
+def delete_student():
+    form = DeleteForm()
+
+    if form.validate_on_submit():
+        student_id = form.student_id.data
+        student = Students.query.get(student_id)
+        db.session.delete(student)
+        db.session.commit()
+
+        flash('Student Successfully Deleted.')
+
+        return redirect(url_for("students.delete_student"))
+
+    return render_template("delete.html", form=form)
+
 
