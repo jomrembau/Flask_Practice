fbea211b9d3fa8af2a8c13debda75c1f2864918b
jomrembau <jomir.bautista@gmail.com>
2026-08-28 08:26:10 +0200

    @user.route /account

--- Social Company Blog/companyblog/users/views.py
+++ Social Company Blog/companyblog/users/views.py
@@ -50,3 +50,29 @@ def login():
             return redirect(next)
 
     return render_template("login.html", form=form)
+
+@users.route("/account", methods=["GET","POST"])
+@login_required
+def account():
+
+    form = UpdateUserForm()
+    if form.validate_on_submit():
+
+        if form.picture.data: # if user uploaded a photo. picture variable from forms.py
+            username = current_user.username
+            pic = add_profile_pic(form.picture.data,username) # add_profile_pic method from picture_handler
+            current_user.profile_image = pic
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("User Account updated")
+        return redirect(url_for("user.account"))
+
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    profile_image = url_for("static", filename="profile_pics/"+ current_user.profile_image)
+
+    return render_template("account.html", profile_image=profile_image, form=form)
