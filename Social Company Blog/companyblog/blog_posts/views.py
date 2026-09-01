26a717a025918eb749b19cea92c46e7acbeb48db
jomrembau <jomir.bautista@gmail.com>
2026-09-01 09:14:45 +0200

    def delete_post

--- Social Company Blog/companyblog/blog_posts/views.py
+++ Social Company Blog/companyblog/blog_posts/views.py
@@ -66,3 +66,14 @@ def update(blog_post_id):
     return render_template("create_post.html", title="updating" ,form=form)
 
 #DELETE
+@blog_posts.route("/<int:blog_post_id/delete", methods=["GET","POST"])
+@login_required
+def delete_post(blog_post_id):
+    blog_post = BlogPost.query.get_or_404(blog_post_id)
+    if blog_post.author != current_user:
+        abort(403)
+
+    db.session.delete(blog_post)
+    db.session.commit()
+    flash("Blog Post Deleted")
+    return redirect(url_for("core.index"))
