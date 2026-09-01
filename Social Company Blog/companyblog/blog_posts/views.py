3232965803557a442124e0e29143a2f99790ec66
jomrembau <jomir.bautista@gmail.com>
2026-09-01 08:49:13 +0200

    Update Section

--- Social Company Blog/companyblog/blog_posts/views.py
+++ Social Company Blog/companyblog/blog_posts/views.py
@@ -1,4 +1,4 @@
-from flask import render_template,url_for,flash, request, redirect, Blueprint
+from flask import render_template,url_for,flash, request, redirect, Blueprint, abort
 from flask_login import  current_user,login_required
 from companyblog import db
 from companyblog.models import BlogPost
@@ -40,4 +40,29 @@ def blog_post(blog_post_id):
 
 
 #UPDATE
-#DALETE
+@blog_posts.route("/<int:blog_post_id>/update", methods=["GET","POST"])
+@login_required
+def update(blog_post_id):
+    blog_post = BlogPost.query.get_or_404(blog_post_id)
+
+    if blog_post.author != current_user:
+        abort(403)
+
+    form = BlogPostForm()
+
+    if form.validate_on_submit():
+        blog_post.title = form.title.data #see difference vs create above.
+        blog_post.text = form.text.data
+        # no need to db.session.add as it it only an update
+        db.session.commit()
+        flash("Blog Post Updated")
+
+        return redirect(url_for("blog_post.blog_post", blog_post_id=blog_post_id))
+
+    elif request.method == "GET":
+        form.title.data = blog_post.title
+        form.text.data = blog_post.text
+
+    return render_template("create_post.html", title="updating" ,form=form)
+
+#DELETE
